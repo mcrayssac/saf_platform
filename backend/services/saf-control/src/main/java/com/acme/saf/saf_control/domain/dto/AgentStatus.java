03a679c93663c0b298c5a5a19cef2909c9ab5655
floflo95520 <floflo042004@gmail.com>
2025-11-02 13:27:43 +0100

    Test avec ajout de deux classes:
    AgentStatus : Enumération pour définir tous les statuts possibles d'un
    agent (Actif, Inactif, Inconnu)
    
    AgentType : Enumération pour définir tous les types possibles d'un agent
    (Client, Ville, Capteur)

---
+++ backend/services/saf-control/src/main/java/com/acme/saf/saf_control/domain/dto/AgentStatus.java
@@ -0,0 +1,7 @@
+package com.acme.saf.saf_control.domain.dto;
+
+public enum AgentStatus {
+    ACTIVE,
+    INACTIVE,
+    UNKNOWN
+}
---
+++ backend/services/saf-control/src/main/java/com/acme/saf/saf_control/domain/dto/AgentType.java
@@ -0,0 +1,7 @@
+package com.acme.saf.saf_control.domain.dto;
+
+public enum AgentType {
+    CLIENT,
+    VILLE,
+    CAPTEUR
+}
