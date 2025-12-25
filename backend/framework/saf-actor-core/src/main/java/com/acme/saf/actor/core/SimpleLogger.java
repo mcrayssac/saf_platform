64f2d36ef4de0ec64a9bde0085e290e7704393d1
unknown <demoraisth@cy-tech.fr>
2025-12-25 01:29:39 +0100

    Implémentation de la création dun fichier de logs qui recensera tous les logs des actions référentes sans supprimer les logs de debuggage dans la console

--- backend/framework/saf-actor-core/src/main/java/com/acme/saf/actor/core/SimpleLogger.java
+++ backend/framework/saf-actor-core/src/main/java/com/acme/saf/actor/core/SimpleLogger.java
@@ -1,5 +1,9 @@
 package com.acme.saf.actor.core;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
@@ -7,6 +11,9 @@ public class SimpleLogger implements Logger {
 
     private final String actorName;
     private static final DateTimeFormatter FMT = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
+
+    // fichier où tous les logs sont stockés
+    private static final Path LOG_FILE_PATH = Path.of("saf-runtime.log");
 
     public SimpleLogger(String actorName) {
         this.actorName = actorName;
@@ -23,25 +30,47 @@ public class SimpleLogger implements Logger {
         );
     }
 
+    private synchronized void writeToFile(String content) {
+        try {
+            // On écrit aussi dans la console pour que debugger
+            System.out.println(content);
+
+            // Écriture dans le fichier (Mode append pour ne pas écraser les logs précédents mais à voir)
+            Files.writeString(LOG_FILE_PATH, content + System.lineSeparator(),
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.WRITE,
+                    StandardOpenOption.APPEND);
+            
+        } catch (IOException e) {
+            // Si le log échoue on l'affiche en erreur
+            System.err.println("ERREUR LOGGING: Impossible d'écrire dans " + LOG_FILE_PATH);
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void info(String msg) {
-        System.out.println(format("INFO", msg));
+        writeToFile(format("INFO", msg));
     }
 
     @Override
     public void warning(String msg) {
-        System.out.println(format("WARN", msg));
+        writeToFile(format("WARN", msg));
     }
 
     @Override
     public void error(String msg, Throwable t) {
-        System.err.println(format("ERROR", msg));
+        writeToFile(format("ERROR", msg));
         if (t != null) {
-            t.printStackTrace(System.err);
+            StringBuilder sb = new StringBuilder();
+            for (StackTraceElement elem : t.getStackTrace()) {
+                sb.append("\t").append(elem.toString()).append(System.lineSeparator());
+            }
+            writeToFile(sb.toString());
         }
     }
 
     public void logEvent(ActorLifecycleEvent event) {
-        System.out.println(format("EVENT", event.toString()));
+        writeToFile(format("EVENT", event.toString()));
     }
 }
